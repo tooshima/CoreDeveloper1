7e9ee733a9458032e52d7c34f9169655a3178738
Takenori Oshima <takenori.oshima@tri-ad.global>
2019-10-16 17:09:58 +0900

    Modified FUNCTION1 naming - from function name, it is difficult to understand what this is - add specific value to argument because it may change the specific value - argument l is difficult to understand what type this is - we should not use like o

--- week2/exercise.py
+++ week2/exercise.py
@@ -8,22 +8,33 @@
 
 # FUNCTION 1: Check for a specific value in a list
 
-def checkNums(l):
-    if 1 in l:
-        print "There is a 1"
+
+def find_specific_value_in_list(find_value, input_list):
+    if find_value in input_list:
+        print("There is a 1")
         return
     else:
-        for o in l:
-            print o
+        for item in input_list:
+            print(item)
         return
 
 # FUNCTION 2: Pick out even numbers
+
 
 def evenStevens(thing):
     thanos = []
     for avenger in thing:
         if (avenger % 2) == 0:
             thanos.append(avenger)
-    print "I AM INEVITABLE"
-    print thanos
+    print("I AM INEVITABLE")
+    print(thanos)
     return thanos
+
+
+if __name__ == "__main__":
+    target_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    find_value = 1
+    find_specific_value_in_list(find_value, target_list)
+
+    find_value = 100
+    find_specific_value_in_list(find_value, target_list)
